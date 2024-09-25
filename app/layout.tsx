38896bd550cac9c63fb242a210aba398eebd78bb
Coderman47 <askinglucho@gmail.com>
2024-09-25 12:50:28 -0300

    learning how the Link component works

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
-import { montserrat } from './ui/fonts';
-import './ui/global.css';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) { 
+}) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>
-        {children}
-        <footer className='py-10 flex justify-center items-center'>
-          Hecho con amor por la gente de Vercel
-        </footer>
-      </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
-import { Lusitana, Montserrat } from 'next/font/google'
-
-export const montserrat = Montserrat({subsets: ['latin']})
-
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
 export const lusitana = Lusitana({
-    weight: ['400', '700'], 
-    subsets: ['latin']
-})
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
